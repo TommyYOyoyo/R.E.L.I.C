57ba27d96942edf8ea02209f5397c8fcbe167591
TommyYOyoyo <79941840+TommyYOyoyo@users.noreply.github.com>
2025-04-10 18:19:39 -0400

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,30 @@
 import "../css/style.css";
 import Phaser from "phaser";
 
+const speedDown = 300;
+let platforms;
+let bgTiles;
+
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight,
+    mapWidth: window.innerWidth * 5,
+    mapHeight: window.innerHeight * 3,
 };
-
-const speedDown = 300;
 
 class GameScene extends Phaser.Scene {
     constructor() {
         super("scene-game");
+        this.player;
     }
 
     preload() {
-        this.load.image("bg", "../../public/assets/img/background.png");
+        this.load.image("bg", "../../public/assets/img/Layers/back.png");
     }
 
     create() {
-        this.add.image(0, 0, "bg");
+        const bg1 = this.add.image(0, 400, "bg");
+        bg1.scale = 3;
     }
 
     update() {
