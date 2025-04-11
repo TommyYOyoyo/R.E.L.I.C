38d045e4809388867a330979ade3ff44af59faac
TommyYOyoyo <79941840+TommyYOyoyo@users.noreply.github.com>
2025-04-10 20:27:55 -0400

    added bg

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,13 @@ import Phaser from "phaser";
 
 const speedDown = 300;
 let platforms;
-let bgTiles;
+const grassTileWidth = 64;
+const grassTileHeight = 69;
+let bgTiles = [];
+const bgTileWidth1 = 960 * 3;
+const bgTileHeight1 = 272 * 3;
+const bgTileWidth2 = 1152 * 3;
+const bgTileHeight2 = 272 * 3;
 
 const sizes = {
     width: window.innerWidth,
@@ -18,13 +24,32 @@ class GameScene extends Phaser.Scene {
         this.player;
     }
 
+    // Preload all assets (init)
     preload() {
-        this.load.image("bg", "../../public/assets/img/Layers/back.png");
+        this.load.image("bgBack", "../../public/assets/img/Layers/back.png");
+        this.load.image("bgFront", "../../public/assets/img/Layers/front.png");
+        const grassTile1 = this.load.image("grassTile1", "../../public/assets/img/Tiles/grassTile1.png");
+        const grassTile2 = this.load.image("grassTile2", "../../public/assets/img/Tiles/grassTile2.png");
     }
 
+    // Create all game objects
     create() {
-        const bg1 = this.add.image(0, 400, "bg");
-        bg1.scale = 3;
+
+        platforms = this.physics.add.staticGroup();
+
+        // Load background pictures (independent layers)
+        const bgBack = this.add.image(bgTileWidth1 / 2, bgTileHeight1 / 2, "bgBack");
+        const bgFront = this.add.image(bgTileWidth2 / 2, bgTileHeight2 / 2, "bgFront");
+        bgBack.scale = 3;
+        bgFront.scale = 3;
+
+        // Load grass floor
+        for (let i = 0; i < sizes.mapWidth / grassTileWidth; i++) {
+            const grassTile = `grassTile${Math.floor(Math.random() * 2) + 1}`;
+            const grassTileX = i * grassTileWidth;
+            this.add.image(grassTileX, sizes.height - grassTileHeight / 2, grassTile);
+        }
+
     }
 
     update() {
