18569cfb78a24317b6e01477fe78b29eedb61fc2
TommyYOyoyo <79941840+TommyYOyoyo@users.noreply.github.com>
2026-05-25 13:00:42 -0400

    patched a glitch

--- src/js/levels/level5.js
+++ src/js/levels/level5.js
@@ -1,6 +1,10 @@
 /**
- * Level 5 / boss game file
+ * Level 5 game file
  * @author Honglue Zheng
+ * 
+ * @note Improvements from 2025
+ * - Added boss level setup
+ * - Boss, enemies, and interactive objects
  */
 
 import Phaser from "phaser";
@@ -24,15 +28,24 @@ import {
 } from "../knightBoss.js";
 
 function loadAssets(scene) {
+    // Load level-specific assets
+    // Load common assets shared across all levels
     loadCommonAssets(scene);
+
+    // Load tilesets
     scene.load.image("forestSet", "/assets/img/Forest_Pack/extruded-tileset.png");
     scene.load.image("dungeonSet", "/assets/img/Dungeon_Pack/Tileset-extruded.png");
     scene.load.image("ruinSet", "/assets/img/Ruins_Pack/Tileset-extruded.png");
+
+    // Load map
     scene.load.tilemapTiledJSON("map", "/assets/img/maps/l5_map.tmj");
+
+    // Load enemy and boss assets
     loadEnemyAssets(scene);
     loadBossAssets(scene);
 }
 
+// Level 5 scene
 class Level5 extends Phaser.Scene {
     constructor() {
         super("Level5");
@@ -54,41 +67,48 @@ class Level5 extends Phaser.Scene {
     }
 
     preload() {
+        // Preload all assets (init)
         loadAssets(this);
+
+        // Load standard keyboard keys
         loadKeyboardKeys(this);
     }
 
     create() {
+        // Fade in from black over 1 second
         this.cameras.main.fadeIn(2000, 0, 0, 0);
 
+        // Map creation
         const map = this.make.tilemap({ key: "map" });
         const forestSet = map.addTilesetImage("Forest", "forestSet", 16, 16, 1, 2);
         const dungeonSet = map.addTilesetImage("Dungeon", "dungeonSet", 16, 16, 1, 2);
         const extrudedSet = map.addTilesetImage("Tileset-extruded", "ruinSet", 16, 16, 1, 2);
 
+        // Loading structures
         const wall0 = map.createLayer("Wall0", [forestSet, dungeonSet, extrudedSet], 0, 0);
         const wall1 = map.createLayer("Wall1", [forestSet, dungeonSet, extrudedSet], 0, 0);
         const ground = map.createLayer("Ground", [forestSet, dungeonSet, extrudedSet], 0, 0);
         const decorations = map.createLayer("Decorations", [forestSet, dungeonSet, extrudedSet], 0, 0);
         const decorations2 = map.createLayer("Decorations 2", [forestSet, dungeonSet, extrudedSet], 0, 0);
 
+        // Scale layers
         [wall0, wall1, ground, decorations, decorations2].forEach((layer) => {
             if (layer) layer.setScale(this.scaleMultiplier).setOrigin(0, 0);
         });
 
+        // Set layer depths
         if (wall0) wall0.setDepth(0);
         if (wall1) wall1.setDepth(1);
         if (ground) ground.setDepth(2);
         if (decorations) decorations.setDepth(3);
         if (decorations2) decorations2.setDepth(4);
 
-        if (wall0) wall0.setCollisionByExclusion([-1], true);
-        if (wall1) wall1.setCollisionByExclusion([-1], true);
         if (ground) ground.setCollisionByExclusion([-1], true);
 
         this.ground = ground;
-        this.playerCollisionLayers = [wall0, wall1, ground];
+        this.playerCollisionLayers = [ground];
 
+        // Create interactive objects from tilemap
         this.vines = map.createFromObjects("Objects", { type: "Ladder" }) || [];
         this.checkpoints = map.createFromObjects("Objects", { type: "Checkpoint" }) || [];
         this.enemySpawns = map.createFromObjects("Objects", { type: "EnemySpawn" }) || [];
@@ -101,30 +121,34 @@ class Level5 extends Phaser.Scene {
             ...(map.createFromObjects("Objects", { type: "end" }) || []),
         ];
 
+        // Spawn all objects (using level loader utility)
         [this.vines, this.checkpoints, this.enemySpawns, this.bossSpawns, this.interactables].forEach((elements) => {
             spawnObjects(elements, this.scaleMultiplier, this);
         });
 
+        // Object groups
         this.climbableGroup = this.physics.add.staticGroup();
         this.checkpointGroup = this.physics.add.staticGroup();
         this.enemySpawnsGroup = this.physics.add.staticGroup();
         this.bossSpawnsGroup = this.physics.add.staticGroup();
         this.interactablesGroup = this.physics.add.staticGroup();
 
+        // Add object collections to physics groups (using level loader utility)
         addToGroup(this.vines, this.climbableGroup);
         addToGroup(this.checkpoints, this.checkpointGroup);
         addToGroup(this.enemySpawns, this.enemySpawnsGroup);
         addToGroup(this.bossSpawns, this.bossSpawnsGroup);
         addToGroup(this.interactables, this.interactablesGroup);
 
+        // Spawn player
         this.playerInstance = new Player(this);
         this.playerInstance.load();
         this.player.setScale(this.scaleMultiplier).setDepth(11);
 
-        this.wallCollider0 = wall0 ? this.physics.add.collider(this.player, wall0) : null;
-        this.wallCollider1 = wall1 ? this.physics.add.collider(this.player, wall1) : null;
+        // Set world bounds
         this.groundCollider = ground ? this.physics.add.collider(this.player, ground) : null;
 
+        // Camera movement and delimitation
         this.physics.world.setBounds(
             0,
             0,
@@ -139,19 +163,15 @@ class Level5 extends Phaser.Scene {
         );
         this.cameras.main.startFollow(this.player, true);
 
-        if (wall0 && this.enemies) this.physics.add.collider(this.enemies, wall0);
-        if (wall1 && this.enemies) this.physics.add.collider(this.enemies, wall1);
         if (ground && this.enemies) this.physics.add.collider(this.enemies, ground);
 
+        // Spawn skeleton and boss
         createSkeleton(this, wall0 || ground, 1, 350);
-        if (wall1 && this.enemies) this.physics.add.collider(this.enemies, wall1);
         if (ground && this.enemies) this.physics.add.collider(this.enemies, ground);
 
         createBossAnimations(this);
         this.boss = spawnBoss(this);
         if (this.boss) {
-            if (wall0) this.physics.add.collider(this.boss, wall0);
-            if (wall1) this.physics.add.collider(this.boss, wall1);
             if (ground) this.physics.add.collider(this.boss, ground);
         }
     }
@@ -159,9 +179,11 @@ class Level5 extends Phaser.Scene {
     update() {
         if (this.isPaused) return;
 
+        // Update game tick
         this.gameTick++;
         if (this.gameTick > 100000) this.gameTick = 0;
 
+        // Update player and enemies
         this.playerInstance.update();
         this.playerInstance.hitboxUpdater();
         updateSkeleton(this);
